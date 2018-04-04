469fbf7409008ed7024955d6d6710325efca5279
randall ishino <rishino24@gmail.com>
2018-04-03 21:01:11 -0700

    note and article schema completed

---
+++ models/note.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+
+var Schema = mongoose.Schema;
+
+var NotesSchema = new Schema({
+    title: String,
+    body: String
+});
+
+var Note = mongoose.model("Note",NotesSchema);
+
+module.exports = Note;
