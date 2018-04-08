a1d9dbffdf01d45cecd02dd66c533adc2c7bf3f3
randall ishino <rishino24@gmail.com>
2018-04-08 14:15:21 -0700

    routes added

---
+++ routes/routes.js
@@ -0,0 +1,133 @@
+//scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+var methodOverride = require("method-override");
+var Article = require('../models/article.js');
+var Note = require('../models/note.js');
+
+module.exports = function(router) {
+
+	router.get("/", function(req, res){
+		Article.find({
+			saved: false
+		}, function(err, dbArticle) {
+		if (err) {
+			res.send(err);
+		}
+
+		else{
+			res.render("home", {article: dbArticle} );
+		}
+		});
+	})
+
+
+
+
+    router.get("/scrape", function(req, res) {
+        // making http request to grab html
+        request("https://www.nytimes.com/", function(error, response, html) {
+          // saving info into cheerio
+        var $ = cheerio.load(html);
+          // looping over each article tag
+          $("article").each(function(i, element) {
+      
+            // Save an empty result object
+            var result = {};
+      
+            // saving all text into result object
+            result.title = $(this).children("h2").text();
+            result.summary = $(this).children(".summary").text();
+            result.link = $(this).children("h2").children("a").attr("href");
+      
+            // Using our Article model, create a new instance
+            var new_Article = new Article(result);
+      
+            // saving data into db using new instance
+            new_Article.save(function(err, res) {
+              // Log any errors
+              if (err) {
+                console.log(err);
+              }
+              else {
+                console.log(res);
+              }
+            });
+          })
+        });
+    });
+
+
+
+
+	// This route renders the saved handledbars page
+  router.get("/saved", function(req, res) {
+  	// res.json('route hit')
+  	 Article.find({saved: true}).populate("notes", 'body').exec(function(err, dbArticle) {
+    if (err) {
+      res.send(err);
+    }
+    else {
+      res.render("saved", {saved: dbArticle});
+    }
+  	});
+  });
+
+  
+  // put route to updated the article to be saved:true
+  router.post("/saved/:id", function(req, res) {
+	  Article.update({_id: req.params.id}, {$set: {saved: true}}, function(err, dbArticle) {
+	    if (err) {
+	      res.send(err);
+	    }
+	    else {
+	      res.redirect("/");
+	    }
+	  });
+	});
+
+	//delete route for articles on the saved page
+	router.post("/delete/:id", function(req, res){
+		 Article.update({_id: req.params.id}, {$set: {saved: false}}, function(err, dbArticle) {
+	    if (err) {
+	      res.send(err);
+	    }
+	    else {
+	      res.redirect("/saved");
+	    }
+	  });
+	})
+
+	//post route for saving a note to an article
+	router.post("/saved/notes/:id", function(req, res) {
+	  var newNote = new Note(req.body);
+	  console.log("new note" + newNote);
+	  newNote.save(function(error, dbNote) {
+	    if (error) {
+	      res.send(error);
+	    }
+	    else {
+	      Article.findOneAndUpdate({_id: req.params.id}, { $push: { "notes": dbNote._id } }, { new: true }).exec(function(err, newdoc) {
+	        if (err) {
+	          res.send(err);
+	        }
+	        else {
+	          res.redirect("/saved");
+	        }
+	      });
+	    }
+	  });
+	});
+
+	// delete route to delete a note
+	router.post("/saved/delete/:id", function(req, res) {
+	  Note.remove({_id: req.params.id}, function(err, dbNote){
+	    if (err) {
+	      res.send(err);
+	    }
+	    else {
+	      res.redirect("/saved");
+	    }
+	  });
+	});
+}
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+// setup required packages for use
+var exphbs = require("express-handlebars");
 var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var path = require('path'); 
 var request = require('request');
+var methodOverride = require("method-override");
+
 // scraping packages
 var cheerio = require("cheerio");
 
@@ -11,62 +14,79 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 // If deployed, use the deployed database. Otherwise use the local mongoScraper database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper";
+
+// require models 
 var Note = require("./models/note.js");
 var Article = require("./models/article.js");
+
+
+// Set up an Express Router
+var router = express.Router();
+
+// Require our routes file pass our router object
+require("./routes/routes.js")(router);
+
+// Have every request go through our router middleware
+app.use(router);
+
+// establish mongoose connection
 var db = mongoose.connection;
+
 // Use morgan logger for logging requests
 app.use(logger("dev"));
+
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: false }));
+
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 app.use(express.static("views"));
 
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Use promise syntax
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
+db.once("open", function() {
+    console.log("Mongoose connection successful.");
+  });
+  
+
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
   });
 
-// app.get("/", function(req,res) {
-//     res.sendFile('index.html');
-// });
-
-
 app.get("/scrape", function(req, res) {
-    // First, we grab the body of the html with request
+    // making http request to grab html
     request("https://www.nytimes.com/", function(error, response, html) {
-      // Then, we load that into cheerio and save it to $ for a shorthand selector
-      var $ = cheerio.load(html);
-      // Now, we grab every h2 within an article tag, and do the following:
+      // saving info into cheerio
+    var $ = cheerio.load(html);
+      // looping over each article tag
       $("article").each(function(i, element) {
   
         // Save an empty result object
         var result = {};
   
-        // Add the title and summary of every link, and save them as properties of the result object
+        // saving all text into result object
         result.title = $(this).children("h2").text();
         result.summary = $(this).children(".summary").text();
         result.link = $(this).children("h2").children("a").attr("href");
   
-        // Using our Article model, create a new entry
-        // This effectively passes the result object to the entry (and the title and link)
+        // Using our Article model, create a new instance
         var new_Article = new Article(result);
   
-        // Now, save that entry to the db
-        new_Article.save(function(err, doc) {
+        // saving data into db using new instance
+        new_Article.save(function(err, res) {
           // Log any errors
           if (err) {
             console.log(err);
           }
-          // Or log the doc
           else {
-            console.log(doc);
+            console.log(res);
           }
         });
       })
